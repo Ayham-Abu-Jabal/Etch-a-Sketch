f83748f638ad10cbdb85216497776775e2004ce7
Ayham Abu Jabal <ayham.abujabal2007@gmail.com>
2026-09-22 22:09:53 +0300

    Added grid UI

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
-const gridCon = document.set
-const div = document.createAttribute("div");
+const container = document.getElementById("grid-container");
+function createGrid(){
+    for(let i = 0; i <16*16; i++){
+        const cell = document.createElement("div");
+        cell.classList.add("grid-cell");
+        container.appendChild(cell);
+    }
+}
+createGrid();
+const grid = document.querySelector(".grid-cell");
+grid.addEventListener("mouseenter", ()=> {
+    grid.style.color = "black";
+});
