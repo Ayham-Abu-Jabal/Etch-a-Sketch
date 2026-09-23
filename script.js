fd37eca01abe708d5a8bb5744cb66300e1bb7138
Ayham Abu Jabal <ayham.abujabal2007@gmail.com>
2026-09-23 09:27:20 +0300

    can color in the grid and added a button

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
-const container = document.getElementById("grid-container");
-function createGrid(){
-    for(let i = 0; i <16*16; i++){
+function createGrid(size) {
+    const container = document.querySelector("#grid-container");
+    for (let i = 0; i < size; i++) {
         const cell = document.createElement("div");
         cell.classList.add("grid-cell");
+        cell.addEventListener("mouseover", ()=>{cell.style.backgroundColor = "black";});
         container.appendChild(cell);
     }
 }
-createGrid();
-const grid = document.querySelector(".grid-cell");
-grid.addEventListener("mouseenter", ()=> {
-    grid.style.color = "black";
-});
+createGrid(16);
+
